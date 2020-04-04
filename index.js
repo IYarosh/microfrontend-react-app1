b682e5b14bb011adcf7b9d567aba11a0d9a2f730
IYarosh <IYarosh@qulix.com>
2020-04-04 19:03:14 +0300

    Change export format

--- index.js
+++ index.js
@@ -9,29 +9,28 @@ function App() {
 
 // ReactDOM.render(<App />, document.getElementById('app1-container'))
 
-module.exports = {
-    bootstrap: function(props) {
-        console.log('React app 1 bootstrap. Props:', props)
-        return Promise
-            .resolve()
-            .then(() => {
-    
-            })
-    },
-    mount: function(props) {
-        console.log('React app 1 mount. Props:', props);
-        return Promise
-            .resolve()
-            .then(() => {
-                ReactDOM.render(<App />, document.getElementById('app1-container'));
-            })
-    },
-    unmount: function(props) {
-        console.log('React app 1 unmount. Props:', props);
-        return Promise
-            .resolve()
-            .then(() => {
-                ReactDOM.unmountComponentAtNode(document.getElementById('app1-container'));
-            })
-    }
+module.exports.bootstrap = function(props) {
+    console.log('React app 1 bootstrap. Props:', props)
+    return Promise
+        .resolve()
+        .then(() => {
+
+        })
+}
+
+module.exports.mount = function(props) {
+    console.log('React app 1 mount. Props:', props);
+    return Promise
+        .resolve()
+        .then(() => {
+            ReactDOM.render(<App />, document.getElementById('app1-container'));
+        })
+}
+module.exports.unmount = function(props) {
+    console.log('React app 1 unmount. Props:', props);
+    return Promise
+        .resolve()
+        .then(() => {
+            ReactDOM.unmountComponentAtNode(document.getElementById('app1-container'));
+        })
 }
