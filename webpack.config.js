3969c54c76c02331b827690eb420e074999c23d8
IYarosh <IYarosh@qulix.com>
2020-04-06 10:29:32 +0300

    Change library export

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        library: 'microfrontend-react-app1',
-        libraryTarget: 'var'
+        libraryTarget: 'umd'
     },
     mode: "development",
     plugins: [
